579a09fbdec4abf2ea91658cd7cde9b3a0a658dd
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2024-10-03 20:17:26 -0300

    att menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,10 +3,14 @@ import { NavLink } from "react-router-dom"
 function Menu (){
     return(
         <>
-            <div className="bg-sky-800">
-                <h2 className="nome">Luiz Souza</h2>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/contato'>Contato</NavLink>
+            <div className="bg-blue-700 flex justify-center text-cyan-200 py-8 text-3xl font-semibold ">
+                <div className="flex pl-96">
+                    <h2 className="nome mr-40">Luiz Souza</h2>
+                    <div>
+                        <NavLink className="mx-10" to='/'>Home</NavLink>
+                        <NavLink className="mx-10" to='/contato'>Contato</NavLink>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import Sobre from "../components/Sobre"
 
 function Home (){
     return(
-        <>
+        <div className="bg-black text-zinc-50">
             <Sobre/>
             <Curiosidade/>
-        </>
+        </div>
     )
 }
 export default Home
